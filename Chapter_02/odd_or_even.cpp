e173467ba6ec359c2d8e84706ec56ec568ee31c4
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 23:41:03 +0300

    Add odd or even number program

--- Chapter_02/odd_or_even.cpp
+++ Chapter_02/odd_or_even.cpp
@@ -2,3 +2,22 @@
 // As always, make sure your output is clear and complete. In other words, don’t just output yes or no.
 // Your output should stand alone, like The value 4 is an even number.
 
+#include <iostream>
+
+int main()
+{
+	int value;
+	std::cin >> value;
+
+	if (value % 2 == 0)
+	{
+		std::cout << "The value " << value << " is an even number.\n";
+	}
+	else
+	{
+		std::cout << "The value " << value << " is an odd number.\n";
+	}
+
+	return 0;
+}
+
