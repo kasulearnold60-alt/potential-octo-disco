90c410018e9ebd3dcce85a1e1e885eb41a57e24e
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 23:53:03 +0300

    Implement average speed calculation

--- Chapter_02/speed.cpp
+++ Chapter_02/speed.cpp
@@ -2,3 +2,18 @@
 // Write a program that displays the average speed in miles per hour. (Note that
 // 1 mile is 1.6 kilometers.)
 
+#include <iomanip>
+#include <iostream>
+
+int main()
+{
+	const double distanceInMiles = 14.0 / 1.6;
+	const double timeInHours = (45.0 * 60.0 + 30.0) / 3600.0;
+	const double averageSpeed = distanceInMiles / timeInHours;
+
+	std::cout << std::fixed << std::setprecision(2)
+			  << "Average speed: " << averageSpeed << " miles per hour\n";
+
+	return 0;
+}
+
