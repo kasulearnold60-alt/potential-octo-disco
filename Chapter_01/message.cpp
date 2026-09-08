35e24b6610a74812dcfc2c292ea35e258a5dc3f2
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 22:05:25 +0300

    Complete display three messages exercise

--- Chapter_01/message.cpp
+++ Chapter_01/message.cpp
@@ -1,2 +1,10 @@
-// (Display three different messages) Write a program that displays Welcome to
-// C++, Welcome to OOP, and Programming is fun.
+
+#include <iostream>
+
+int main() {
+	std::cout << "Welcome to C++" << '\n';
+	std::cout << "Welcome to OOP" << '\n';
+	std::cout << "Programming is fun" << '\n';
+
+	return 0;
+}
