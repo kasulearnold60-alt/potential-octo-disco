73551aaaab066b8aec0f4fce96b55aaed3a05a66
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 23:35:12 +0300

    Implement math expression calculation

--- Chapter_02/math_expression.cpp
+++ Chapter_02/math_expression.cpp
@@ -1,3 +1,13 @@
 // (Compute expressions) Write a program that displays the result of;
 // (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
 
+#include <iomanip>
+#include <iostream>
+
+int main() {
+	const double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+
+	std::cout << std::fixed << std::setprecision(2) << result << '\n';
+	return 0;
+}
+
