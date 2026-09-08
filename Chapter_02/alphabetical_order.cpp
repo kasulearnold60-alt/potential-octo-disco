146804731ecf3bfe4ce366998fe27e732f08e7fb
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 22:39:04 +0300

    Rewrite alphabetical order for strings

--- Chapter_02/alphabetical_order.cpp
+++ Chapter_02/alphabetical_order.cpp
@@ -1,3 +1,35 @@
-// Rewrite the ascending.cpp program to process stings instead.
-// So, if the user enters the values Electrical Eng., Computer Eng., Biomedical Eng.,
-// the output should be Biomedical Eng., Computer Eng., Electrical Eng..
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string a, b, c;
+
+	cout << "Enter three strings:\n";
+	getline(cin, a);
+	getline(cin, b);
+	getline(cin, c);
+
+	if (a > b) {
+		string temp = a;
+		a = b;
+		b = temp;
+	}
+
+	if (a > c) {
+		string temp = a;
+		a = c;
+		c = temp;
+	}
+
+	if (b > c) {
+		string temp = b;
+		b = c;
+		c = temp;
+	}
+
+	cout << a << ", " << b << ", " << c << endl;
+
+	return 0;
+}
