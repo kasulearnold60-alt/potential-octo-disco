21c334fa5aa1978c15474d500f81f5bdb385c9cc
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 23:48:01 +0300

    Add population projection program

--- Chapter_02/population.cpp
+++ Chapter_02/population.cpp
@@ -5,4 +5,24 @@
 // One new immigrant every 45 seconds
 // Write a program to display the population for each of the next five years. Assume the current population is 312032486 and one year has 365 days.
 
+#include <iostream>
+
+int main()
+{
+	constexpr long long secondsPerYear = 365LL * 24 * 60 * 60;
+	long long population = 312032486;
+
+	const long long birthsPerYear = secondsPerYear / 7;
+	const long long deathsPerYear = secondsPerYear / 13;
+	const long long immigrantsPerYear = secondsPerYear / 45;
+
+	for (int year = 1; year <= 5; ++year)
+	{
+		population += birthsPerYear - deathsPerYear + immigrantsPerYear;
+		std::cout << "Year " << year << ": " << population << '\n';
+	}
+
+	return 0;
+}
+
 
