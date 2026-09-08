03a0d2b55cb4baa596507e2149b090a0e5803679
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 22:59:39 +0300

    Implement emissions data table

--- Chapter_02/emissions.cpp
+++ Chapter_02/emissions.cpp
@@ -12,3 +12,32 @@
 // 08/8/2026      40
 // 10/8/2026      60
 // 12/8/2027      90
+
+#include <iomanip>
+#include <iostream>
+
+int main() {
+	const int record_count = 3;
+	int days[record_count];
+	int months[record_count];
+	int years[record_count];
+	int quantities[record_count];
+
+	std::cout << "Enter the emission data for " << record_count << " records.\n";
+	for (int record = 0; record < record_count; ++record) {
+		std::cout << "Record " << record + 1
+				  << " (day month year quantity): ";
+		std::cin >> days[record] >> months[record]
+				  >> years[record] >> quantities[record];
+	}
+
+	std::cout << "\nDate\t\tQuantity\n";
+	for (int record = 0; record < record_count; ++record) {
+		std::cout << std::setfill('0') << std::setw(2) << days[record]
+				  << std::setfill(' ') << '/' << months[record]
+				  << '/' << years[record] << "\t\t"
+				  << quantities[record] << '\n';
+	}
+
+	return 0;
+}
