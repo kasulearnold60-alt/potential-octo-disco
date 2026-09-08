6d7527b312b8385ce9723fdb85dd9f057279021d
kasulearnold60-alt <kasulearnold60@gmail.com>
2026-09-08 22:45:44 +0300

    Implement miles to kilometers conversion

--- Chapter_02/convert_miles.cpp
+++ Chapter_02/convert_miles.cpp
@@ -1,3 +1,13 @@
-// Write a program in C++ that converts from miles to kilometers.
-// Your program should have a reasonable prompt for the user to enter a number of miles.
-// Hint: A mile is 1.609 kilometers.
+#include <iostream>
+
+int main() {
+	double miles;
+
+	std::cout << "Enter the number of miles: ";
+	std::cin >> miles;
+
+	const double kilometers = miles * 1.609;
+	std::cout << miles << " miles is " << kilometers << " kilometers.\n";
+
+	return 0;
+}
